19494566f85229cf803e2de2dc57a265ade6e4cb
pino0701 <Tsuyoshi.Kitano+pino0701@gmail.com>
2022-12-08 12:14:38 +0000

    sticky footerのサンプル

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
+import { Box } from '@mui/material';
+
 function App() {
   return (
-    <div>
-      <div>{import.meta.env.VITE_APP_TITLE}</div>
-
-      <h1>Vite + React + TypeScript</h1>
-      <div>version</div>
-      <p>
-        <li>vite@3.2.5</li>
-        <li>react@18.2.0</li>
-        <li>typescript@4.9.3</li>
-        <li>eslint@8.29.0</li>
-        <li>stylelint@14.16.0</li>
-        <li>eslint@8.29.0</li>
-      </p>
-      <div>scripts</div>
-      <p>
-        <li>yarn dev</li>
-        <li>yarn dev</li>
-        <li>yarn build</li>
-        <li>yarn preview</li>
-        <li>yarn eslint</li>
-        <li>yarn eslint:fix</li>
-        <li>yarn stylelint</li>
-        <li>yarn stylelint:fix</li>
-        <li>yarn lint</li>
-        <li>yarn lint:fix</li>
-        <li>yarn format</li>
-        <li>yarn fix</li>
-      </p>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
+      <Box
+        sx={{
+          textAlign: 'center',
+          py: 3,
+          backgroundColor: 'bisque',
+        }}
+      >
+        header area
+      </Box>
+      <Box>
+        <p>react + typescript + mui v5によるstiky footerの実装</p>
+        <p>コンテンツが少ない場合に画面の下にフッターが表示される</p>
+      </Box>
+      <Box
+        sx={{
+          textAlign: 'center',
+          mt: 'auto',
+          backgroundColor: 'lightgray',
+        }}
+      >
+        footer area
+      </Box>
+    </Box>
   );
 }
 
